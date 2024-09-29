ea0629192f9cfcf5271027fe87a51e14204bce11
Aditya Jambhale <jambhaleaditya91@gmail.com>
2024-09-29 12:34:55 +0530

    final build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Github-Resources/',
-
+  // Uncomment the next line if deploying to a sub-path
+  // base: '/<repo-name>/', // Not needed for Vercel
 });
