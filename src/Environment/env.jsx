c600c900fc0d3ebd1f9618734ad84528e1f29b70
Aditya Jambhale <jambhaleaditya91@gmail.com>
2024-09-29 15:48:33 +0530

    Deployment

--- src/Environment/env.jsx
+++ src/Environment/env.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Env from './../components/env'
+import Navbar from './../components/Navbar'
+import Env from './../components/env';
+
 function env() {
   return (
    <>
