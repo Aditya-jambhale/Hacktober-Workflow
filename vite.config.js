223e81012452cf9f0d7e8b6c46863d1b62e78391
Aditya Jambhale <jambhaleaditya91@gmail.com>
2024-09-29 13:36:18 +0530

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   // Uncomment the next line if deploying to a sub-path
-   base: '/Github-Resources/', 
+   
 });
